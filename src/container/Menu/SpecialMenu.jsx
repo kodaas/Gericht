6943b1db9414fa11e763e80de8cb0d783f0c8558
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-09-19 23:58:18 +0000

    Updated Special Menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,7 +7,7 @@ import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
-    <div className="app__specialMenu" style={{textAlign: 'center'}}>
+    <div className="app__specialMenu">
       <SubHeading title="Menu That Fit Your Palatte" />
       <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
