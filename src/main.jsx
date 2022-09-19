19475ffc5b155f7562e6cd5d8fc0dbdba4b9c8e0
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-09-19 23:30:29 +0000

    Completed Intro Video

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client'
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = document.getElementById('root');
+createRoot(root).render(<App />)
 
