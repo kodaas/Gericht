4e637035c869163f549a9ef0126fed9b42005931
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-09-20 01:45:45 +0000

    added newletter section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi'
+
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    {/* <FooterOverlay /> */}
+    <Newsletter />
   </div>
 );
 
